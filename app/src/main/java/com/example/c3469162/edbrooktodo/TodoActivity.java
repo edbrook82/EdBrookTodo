53b4cf462bf802e127740475909f9b31cb1af0ce
Ed Brook <e.brook7292@student.leedsbeckett.ac.uk>
2017-10-06 15:12:59 +0100

    Fixed rotation & added logs to lifecycle methods

--- app/src/main/java/com/example/c3469162/edbrooktodo/TodoActivity.java
+++ app/src/main/java/com/example/c3469162/edbrooktodo/TodoActivity.java
@@ -3,11 +3,21 @@ package com.example.c3469162.edbrooktodo;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class TodoActivity extends AppCompatActivity {
+
+    // In case of state change, due to rotating the phone
+    // store the mTodoIndex to display the same mTodos element post state change
+    // N.B. small amounts of data, typically IDs can be stored as key, value pairs in a Bundle
+    // the alternative is to abstract view data to a ViewModel which can be in scope in all
+    // Activity states and more suitable for larger amounts of data
+    public static final String TODO_INDEX = "todo_index";
+
+    private static final String TAG = "TodoActivity";
 
     private String[] mTodos;
     private int mTodoIndex = 0;
@@ -18,6 +28,8 @@ public class TodoActivity extends AppCompatActivity {
         // call the super class onCreate to complete the creation of activity like
         // the view hierarchy
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG, " *** Just to say the PC is in onCreate!");
 
         // set the user interface layout for this Activity
         // the layout file is defined in the project res/layout/activity_todo.xml file
@@ -31,12 +43,18 @@ public class TodoActivity extends AppCompatActivity {
         Resources res = getResources();
         mTodos = res.getStringArray(R.array.todo);
         // display the first task from mTodo array in the TodoTextView
+
+        // check for saved state due to changes such as rotation or back button
+        // and restore any saved state such as the todo index
+        if (savedInstanceState != null) {
+            mTodoIndex = savedInstanceState.getInt(TODO_INDEX, 0);
+        }
         TodoTextView.setText(mTodos[mTodoIndex]);
 
         Button buttonNext = (Button) findViewById(R.id.buttonNext);
         Button buttonPrev = (Button) findViewById(R.id.buttonPrev);
 
-        // OnClick listener for the  Next button
+        // OnClick listener for the Next button
         buttonNext.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
@@ -45,6 +63,7 @@ public class TodoActivity extends AppCompatActivity {
             }
         });
 
+        // OnClick listener for the Previous button
         buttonPrev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,4 +73,46 @@ public class TodoActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "PC is in onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "PC is in onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "PC is in onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "PC is in onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "PC is in onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "PC is in onDestroy");
+    }
+
+    // Override to write the value of mTodoIndex into the Bundle with TODO_INDEX as its key
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(TODO_INDEX, mTodoIndex);
+    }
 }
